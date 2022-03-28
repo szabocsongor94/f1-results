f875606a908d45ae2e5370c1dfb6d24af316de02
Csongor-Szabo <szabo.csongor94@gmail.com>
2022-03-28 14:42:35 +0200

    finish

--- src/app/podium/podium.component.ts
+++ src/app/podium/podium.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-podium',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PodiumComponent implements OnInit {
 
+  @Input() podium: string[] = [];
+  @Output() closed = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  close(){
+    this.closed.emit();
   }
 
 }
